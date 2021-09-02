a1c3f1e333e3fc0e4258299ad4e985794069ccd3
Ika Amin Triyuliyanti <ikatriyuli@gmail.com>
2021-09-02 18:43:12 +0700

    update-code

---
+++ src/components/agents/home/index.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import NoRequest from 'components/agents/home/dashboard/NoRequest'
+import DetailRequest from 'components/agents/home/dashboard/DetailRequest'
+
+function Home() {
+    return (
+    //   <EmptyRequest />
+    <DetailRequest />
+    )
+}
+
+export default Home
--- src/components/agents/home/part/noRequest.jsx
+++ src/components/agents/home/part/noRequest.jsx
@@ -4,13 +4,13 @@ import imgDoc from 'assets/img/icons/EmptyTransaction.svg';
 function NoRequest() {
     return (
         <div>
-            <section className="home-container">
-                <div className="home-content">
+            <section className="container">
+                <div className="content">
                     <div className="column-no-data">
                         <img src={imgDoc} alt="no image"
                             style={{ width: '309px', height: '226px', left: '630px', top: '292px' }}
                         />              
-                <h3 className="home-no-request">Saat ini belum ada request transaksi</h3>
+                <h3 className="no-request">Saat ini belum ada request transaksi</h3>
                 </div>
             </div>
         </section>
