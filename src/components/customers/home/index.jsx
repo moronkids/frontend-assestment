5b4e458a09276ee4965f05a61ed66232a226f673
Sulistyo Ardani <sulistyoardani@Sulistyos-MacBook-Pro.local>
2021-09-03 08:52:47 +0700

    force to push

---
+++ src/components/customers/home/index.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import NoTransaction from 'components/customers/home/parts/NoTransaction'
+import DetailTransaction from 'components/customers/home/parts/DetailTransaction'
+
+function Home() {
+    return (
+    //   <NoTransaction />
+    <DetailTransaction />
+    )
+}
+
+export default Home
