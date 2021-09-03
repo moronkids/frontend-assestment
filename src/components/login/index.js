4378e0d09b8d8f10869e59287d8b860e35b81547
Sulistyo Ardani <sulistyoardani@Sulistyos-MacBook-Pro.local>
2021-09-03 09:45:41 +0700

    check

--- src/components/login/index.js
+++ src/components/login/index.js
@@ -1,3 +1,4 @@
+/* eslint-disable jsx-a11y/alt-text */
 import React from 'react'
 import Forms from 'components/login/parts/forms';
 import BubbleBtm from 'assets/img/loginPage/bubble-btm.svg';
@@ -14,7 +15,7 @@ function Index() {
             {/* <div style={{maxWidth: 'calc((100vw - 27.125em) /4)', maxHeight: 'calc((100vh. - 21.7em) / 4)'}}> */}
             {/* <div className="bubble-btm d-flex align-items-end position-absolute"/> */}
             <img src={BubbleTop} className="bb-top position-absolute" style={{top: '0', right: '0'}}/>
-            <img src={BubbleBtm} className="bb-btm position-absolute" style={{bottom: '0', left: '0', maxWidth: 'calc((100vw - 27.125em) /4)', maxHeight: 'calc((100vh. - 21.7em) / 4)'}}/>
+            <img src={BubbleBtm} className="bb-btm position-absolute" style={{bottom: '0', left: '0'}}/>
             {/* </div> */}
             {/* bubble */}
             <Forms/>
