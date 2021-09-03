7271a2bcd500fe018b8f53e7c591e9e8a8e2e3f8
wadeveloper2020 <wa.dev2020@gmail.com>
2021-09-03 10:14:30 +0700

    update

--- src/pages/customers/index.js
+++ src/pages/customers/index.js
@@ -1,11 +1,11 @@
 import React from 'react'
 import Customer from 'components/customers';
-import Agent from 'components/agents/profile';
+// import Agent from 'components/agents/profile';
 function Customers() {
     return (
         <>
             <Customer/>
-            <Agent/>
+            {/* <Agent/> */}
         </>
     )
 }
