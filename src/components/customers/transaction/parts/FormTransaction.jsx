e104875cc53465d2773a14450facdc94f32ea851
teguhkrniawan <teguhkrniawan@gmail.com>
2021-09-03 14:57:21 +0700

    fix: Membuat tampilan new transaction

--- src/components/customers/transaction/parts/FormTransaction.jsx
+++ src/components/customers/transaction/parts/FormTransaction.jsx
@@ -11,7 +11,7 @@ function FormTransaction() {
                             <h2 className="judul">JENIS TRANSAKSI</h2>
                         </div>
                         <div className="col-lg-6">
-                            <div className="jenis-layanan">
+                            <div className="jenis-layanan float-right">
                                 Setoran Pinjaman
                             </div>
                         </div>
@@ -21,31 +21,38 @@ function FormTransaction() {
                         <div className="baris row">
                             <div className="col-lg-8">
                                 <label className="form-control-label" htmlFor="nominal-transaksi">Nominal Transaksi</label>
-                                <input type="number" className="form-control-input" name="nominal transaksi" id="nominal-transaksi" placeholder="Jalan Pattimura No.12 Blok C"/>
+                                <input type="number" className="form-control-input" name="nominal transaksi" id="nominal-transaksi" placeholder="Rp. 2.000.000"/>
                             </div>
                         </div>
 
                         <div className="baris">
-                            <label htmlFor="alamat-lengkap">Alamat Lengkap</label>
-                            <input type="text" name="alamat-lengkap" id="alamat-lengkap" placeholder="Jalan Pattimura No.12 Blok C"/>
+                            <label className="form-control-label" htmlFor="alamat-lengkap">Alamat Lengkap</label>
+                            <input type="text" className="form-control-input" name="alamat-lengkap" id="alamat-lengkap" placeholder="Jalan Pattimura No.12 Blok C"/>
                         </div>
 
-                        <div className="baris">
-                            <label htmlFor="provinsi">Provinsi</label>
-                            <select name="provinsi" id="provinsi">
-                                <option value="#">-- PILIH --</option>
-                            </select>
-                            <label htmlFor="kabupaten">Kabupaten</label>
-                            <select name="kabupaten" id="kabupaten">
-                                <option value="#">-- PILIH --</option>
-                            </select>
-                            <label htmlFor="kecamatan">Kecamatan</label>
-                            <select name="kecamatan" id="kecamatan">
-                                <option value="#">-- PILIH --</option>
-                            </select>
+                        <div className="baris daerah row">
+                            <div className="col-lg-4">
+                                <label className="form-control-label" htmlFor="provinsi">Provinsi</label>
+                                <select name="provinsi" id="provinsi">
+                                    <option value="#">-- PILIH --</option>
+                                </select>
+                            </div>
+
+                            <div className="col-lg-4">
+                                <label className="form-control-label"  htmlFor="kabupaten">Kabupaten</label>
+                                <select name="kabupaten" id="kabupaten">
+                                    <option value="#">-- PILIH --</option>
+                                </select>
+                            </div>
+                            <div className="col-lg-4">
+                                <label className="form-control-label" htmlFor="kecamatan">Kecamatan</label>
+                                <select name="kecamatan" id="kecamatan">
+                                    <option value="#">-- PILIH --</option>
+                                </select>
+                            </div>
                         </div>
 
-                        <div className="baris">
+                        <div className="baris btn-cari-agen">
                             <button>Cari Agen</button>
                         </div>
                     </form>
