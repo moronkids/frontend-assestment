376f1d38dc43c29247523f81155cd5e5e678c388
Sulistyo Ardani <sulistyoardani@Sulistyos-MacBook-Pro.local>
2021-09-05 15:19:53 +0700

    fixing container

---
+++ src/dummies/auth/auth.js
@@ -0,0 +1,12 @@
+// export const auth = [{
+//         agent : 'agent@rakamin.com',
+//         customer : 'customer@rakamin.com',
+//         pass : 'root'
+//     }];
+export const auth = () => {
+        return {
+            agent: 'agent@rakamin.com',
+        customer: 'customer@rakamin.com',
+        pass: 'root'
+        }
+    }
