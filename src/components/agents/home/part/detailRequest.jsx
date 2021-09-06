9ac0d1081f3383ac3dcc8d8f667fc115bc8334d2
Ika Amin Triyuliyanti <ikatriyuli@gmail.com>
2021-09-06 09:06:14 +0700

    update

--- src/components/agents/home/part/detailRequest.jsx
+++ src/components/agents/home/part/detailRequest.jsx
@@ -10,79 +10,90 @@ function DetailRequest() {
                     
                     <div className="detail-card">
                         <div className="row label">
+                            
                             <div className="col-lg-6 label-left">
-                                <p>Waktu Request</p>   
-                            </div> 
+                                <h4>Waktu Request</h4>
+                                    <div className="row label-content">
+                                        <div className="col-lg-6 label-left">
+                                            <p>29/08/2021, 13:00 WIB</p>           
+                                        </div>
+                                    </div>
+                            </div>  
+                            
 
                             <div className="col-lg-6 label-right">
-                                <p>Jenis Transaksi</p>
+                                <h4>Jenis Transaksi</h4>
+                                        <div className="row label-content">
+                                        <div className="col-lg-6 label-right">
+                                            <p>Setoran Pinjaman</p>
+                                        </div>
+                                    </div>
                             </div>
-                        </div>
-
-                        <div className="row label-content">
-                            <div className="col-lg-6 label-left">
-                                <p>29/08/2021, 13:00 WIB</p>
-                            </div>
-                            <div className="col-lg-6 label-right">
-                                <p>Setoran Pinjaman</p>
-                            </div>
+                            
+                            
                         </div>
 
                         <hr />
                         <div className="row nominal-content">
                             <div className="col-lg-6 nominal-left">
-                                <p>Nominal Transaksi</p>
+                                <h4>Nominal Transaksi</h4>
                             </div>
                             <div className="col-lg-6 nominal-right">
-                                <p>Rp. 10.000.000</p>
+                                <h5>Rp. 10.000.000</h5>
                             </div>
                         </div>
                         <hr />
 
                         <div className="row label">
                             <div className="col-lg-6 label-left">
-                                <p>Nama Customer</p>
+                                <h4>Nama Customer</h4>
+                                    <div className="row label-content">
+                                        <div className="col-lg-6 label-left">
+                                            <p>Mayasari</p>
+                                        </div>
+                                    </div>
                             </div>
+                        
                             <div className="col-lg-6 label-right">
-                                <p>Alamat Customer</p>
-                            </div>
-                        </div>
-
-                        <div className="row label-content">
-                            <div className="col-lg-6 label-left">
-                                <p>Mayasari</p>
-                            </div>
-                            <div className="col-lg-6 label-right">
-                                <p>Jalan Pajajaran, No.119 Bogor</p>
+                                <h4>Alamat Customer</h4>
+                                    <div className="row label-content">
+                                        <div className="col-lg-6 label-right">
+                                            <p>Jalan Pajajaran, No.119 Bogor</p>
+                                        </div>
+                                    </div>
                             </div>
                         </div>
 
                         <div className="row label">
                             <div className="col-lg-6 label-left">
-                                <p>Hubungi Customer</p>
-                            </div>
-                            <div className="col-lg-6 label-right">
-                                <p>Status</p>
-                            </div>
-                        </div>
+                                <h4>Hubungi Customer</h4>
+                                    <div className="row label-content">
+                                        <div className="col-lg-6 label-left hubungi-wa">
+                                            <img src={IconWa} alt="logo-wa" />
+                                                <a href="https://api.whatsapp.com/send?phone=6282283775912" 
+                                                target="_blank">082283775912</a>
+                                        </div>
+                                    </div>
+                                </div>
 
-                        <div className="row label-content">
-                            <div className="col-lg-6 label-left hubungi-wa">
-                                <img src={IconWa} alt="logo-wa" />
-                                <a href="https://api.whatsapp.com/send?phone=6282283775912" target="_blank">082283775912</a>
+                            <div className="col-lg-6 label-right">
+                                <h4>Status</h4>
+                                    <div className="row label-content">
+                                         <div className="col-lg-6 label-right status-agen">
+                                            <p>Menunggu konfirmasi anda</p>
+                                        </div>
+                                    </div>
                             </div>
-                            <div className="col-lg-6 label-right status-agen">
-                                <p>Menunggu konfirmasi anda</p>
                             </div>
-                        </div>
                         
-                        <div className="baris-button">
+                        <div className="cancel-button">
                             <button className="btn-action-detail">Tolak</button>
                         </div>
 
-                        <div className="baris-button">
+                        <div className="accept-button">
                             <button className="btn-action-detail">Terima</button>
                         </div>
+                        
 
                     </div>
                 </div>
