e592d9f72da6f1fdbc7ae62a8ea83f0eece129e1
Ika Amin Triyuliyanti <ikatriyuli@gmail.com>
2021-09-10 19:07:04 +0700

    up

--- src/components/agents/home/part/detailRequest.jsx
+++ src/components/agents/home/part/detailRequest.jsx
@@ -9,28 +9,22 @@ function DetailRequest() {
                     <h3>Request saat ini</h3>
 
                     <div className="detail-card">
-                        <div className="row label">
-                            <div className="col-lg-6 label-left">
-                                <p>Waktu Request</p>
-                            </div>
 
-                            <div className="col-lg-6 label-right">
-                                <p>Jenis Transaksi</p>
-                            </div>
+                    <div className="row">
+                        <div className="col-lg-6 col-md-12 col-12">
+                            <p className="label-left">Waktu Request</p>
+                            <p className="label-content">19/08/2021, 13:20 WIB</p>
                         </div>
-
-                        <div className="row label-content">
-                            <div className="col-lg-6 label-left">
-                                <p>29/08/2021, 13:00 WIB</p>
+                            
+                            <div className="col-lg-6 col-md-12 col-12">
+                                <p className="label-right">Jenis Transaksi</p>
+                                <p className="label-content">Setoran Tunai</p>
                             </div>
-                            <div className="col-lg-6 label-right">
-                                <p>Setoran Pinjaman</p>
-                            </div>
-                        </div>
+                    </div>
 
                         <hr />
                         <div className="row nominal-content">
-                            <div className="col-lg-6 nominal-left">
+                            <div className="col-lg-6 label-nominal">
                                 <p>Nominal Transaksi</p>
                             </div>
                             <div className="col-lg-6 nominal-right">
@@ -39,55 +33,51 @@ function DetailRequest() {
                         </div>
                         <hr />
 
-                        <div className="row label">
-                            <div className="col-lg-6 label-left">
-                                <p>Nama Customer</p>
+                        <div className="row">
+                            <div className="col-lg-6 col-md-12 col-12">
+                                <p className="label-left">Nama Customer</p>
+                                <p className="label-content">Marisa</p>
                             </div>
-                            <div className="col-lg-6 label-right">
-                                <p>Alamat Customer</p>
-                            </div>
+                                
+                                <div className="col-lg-6 col-md-12 col-12">
+                                    <p className="label-right">Alamat Anda</p>
+                                    <p className="label-content">Jalan Pajajaran No.21</p>
+                                </div>
                         </div>
+                        
 
-                        <div className="row label-content">
-                            <div className="col-lg-6 label-left">
-                                <p>Mayasari</p>
+                        <div className="row">
+                            <div className="col-lg-6 col-md-12 col-12">
+                                <p className="label-left">Hubungi Customer</p>
+                                <div className="label-content hubungi-wa">
+                                    <img src={IconWa} alt="logo-wa" />
+                                    <a href="https://api.whatsapp.com/send?phone=6282283775912" 
+                                            target="_blank" rel="noreferrer">082283775912</a>
+                                </div>
                             </div>
-                            <div className="col-lg-6 label-right">
-                                <p>Jalan Pajajaran, No.119 Bogor</p>
-                            </div>
-                        </div>
+                            
 
-                        <div className="row label">
-                            <div className="col-lg-6 label-left">
-                                <p>Hubungi Customer</p>
-                            </div>
-                            <div className="col-lg-6 label-right">
-                                <p>Status</p>
-                            </div>
-                        </div>
-
-                        <div className="row label-content">
-                            <div className="col-lg-6 label-left hubungi-wa">
-                                <img src={IconWa} alt="logo-wa" />
-                                <a href="https://api.whatsapp.com/send?phone=6282283775912" target="_blank" rel="noreferrer">082283775912</a>
-                            </div>
-                            <div className="col-lg-6 label-right status-agen">
-
+                             <div className="col-lg-6 col-md-12 col-12">
+                                <p className="label-right space">Status</p>
+                                <p className="label-content status-agen">
                                 {
                                     true ? (<p>Menunggu konfirmasi anda</p>) : <p>Agen dalam perjalanan</p>
                                 }
 
+                                </p>
                             </div>
-                        </div>
+                        </div>        
+                        
+                        
 
                         <div className="d-flex" style={{ columnGap: '15px', justifyContent: 'flex-end' }}>
                             <div className="baris-button">
-                                <button className="btn-action-detail">Tolak</button>
+                                <button className="btn-action-detail button_cancel">Tolak</button>
                             </div>
 
                             {
                                 true ? (<div className="baris-button">
-                                    <button className="btn-action-detail">Terima</button>
+                                    <button className="btn-action-detail button_accept">Terima</button>
                                 </div>) : <div className="baris-button">
                                     <button className="btn-action-detail">adadeh</button>
                                 </div>
