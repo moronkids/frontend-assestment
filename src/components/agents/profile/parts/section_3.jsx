316d7c7459ba4995c7d83f41667fed19f8cae605
wadeveloper2020 <wa.dev2020@gmail.com>
2021-09-12 20:37:30 +0700

    feature/profil-agen:edit font style dan button

--- src/components/agents/profile/parts/section_3.jsx
+++ src/components/agents/profile/parts/section_3.jsx
@@ -6,7 +6,7 @@ function section_3(){
         <div className="saldo_container">
             <h5>Jumlah Saldo Minimum</h5>
             <input className="saldo-min" type="text" placeholder="Rp.1.000.000"/>
-            <button className="btn-transaksi-selesai">Transaksi Selesai</button>
+            <button className="btn-transaksi-selesai">Simpan</button>
         </div>
     )
 }
