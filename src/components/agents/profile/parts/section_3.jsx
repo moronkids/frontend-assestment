915864ec626fca5571fcc6972f3d83c7476bbafb
wadeveloper2020 <wa.dev2020@gmail.com>
2021-09-12 21:46:39 +0700

    feature/profil-agen:update

--- src/components/agents/profile/parts/section_3.jsx
+++ src/components/agents/profile/parts/section_3.jsx
@@ -3,11 +3,14 @@ import React from "react";
 
 function section_3(){
     return(
-        <div className="saldo_container">
+        <div>
             <h5>Jumlah Saldo Minimum</h5>
+            <div className="saldo_container">
             <input className="saldo-min" type="text" placeholder="Rp.1.000.000"/>
-            <button className="btn-transaksi-selesai">Simpan</button>
+            <button className="btn-simpan">Simpan</button>
         </div>
+        </div>
+        
     )
 }
 
